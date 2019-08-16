05251191f05d77f4bb39f3868cf79a39b8ec3dda
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:31:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   i=0
-  position = katz_deli.index[i] + 1
+  position = katz_deli[i] + 1
   if katz_deli.length ==0 
     puts "The line is currently empty."
   else 
