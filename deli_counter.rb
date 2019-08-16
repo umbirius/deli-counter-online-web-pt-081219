bd7cdae60fb4a2e330731849f159def6e5fe3da5
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:12:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if array.length ==0 
+    p "The line is currenty empty"
+  else 
+    p 
+  end 
+end 
+
+def take_a_number(katz_deli, name)
+  
