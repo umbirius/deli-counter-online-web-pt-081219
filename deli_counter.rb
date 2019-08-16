82fc7fd300f0ed1a898bbd61c6c4209a4bbcebd2
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:40:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,4 +29,6 @@ def now_serving (katz_deli)
     puts "There is nobody waiting to be served!"
   else 
     puts " Now serving #{katz_deli[0]}."
+  end 
+end 
 #take_a_number(["juliano", "goat"], "leeron")
