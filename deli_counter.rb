afa1ce26a1e63d9ce092605f3f420c50e162e1e3
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:39:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,4 +24,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end 
 
+def now_serving (katz_deli)
+  if katz_deli.length ==0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts " Now serving "
 #take_a_number(["juliano", "goat"], "leeron")
