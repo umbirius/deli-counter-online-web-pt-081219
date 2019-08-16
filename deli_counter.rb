5f4d62963a6def63f4c5ba90ca8e0e7e095799ea
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:29:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 katz_deli = []
 
 def line(katz_deli)
-  position = katz_deli.index(name) + 1
-  i=0
-  if katz_deli.length ==0 
-    puts "The line is currently empty."
-  else 
-    print "The line is currentyly: "
-    while i < katz_deli.length 
-    print "#{position}. #{katz_deli[i]} "
-    i+=1
-  end 
+  #position = katz_deli.index(name) + 1
+  ##i=0
+  #if katz_deli.length ==0 
+   # puts "The line is currently empty."
+  ##else 
+    #print "The line is currentyly: "
+    #while i < katz_deli.length 
+    #print "#{position}. #{katz_deli[i]} "
+    #i+=1
+  #end 
 end 
 
 def take_a_number(katz_deli, name)
