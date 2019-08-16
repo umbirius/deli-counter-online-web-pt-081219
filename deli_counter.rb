6daf92ff6ccc680d21162510d6268af3317edf5e
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 16:20:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
 end 
 
 def take_a_number(katz_deli, name)
-  katz_deli >> name
+  katz_deli << name
   position = katz_deli.index(name) + 1
   puts "#{name}, position #{position}"
 end 
